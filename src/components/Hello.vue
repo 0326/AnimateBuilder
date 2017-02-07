852a6bd1f109f6005d7ffd0cf1e78245bee8826c
0326 <winniringy@gmail.com>
2017-02-08 01:52:22 +0800

    init vue2.0

---
+++ src/components/Hello.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <logo class="logo"></logo>
+    <h2>Animation Builder</h2>
+    <p>Inspired by Adobe Animate CC, Velocity.js and Vue.js.</p>
+    <p>Making the animation workflow between UED and FE easier.</p>
+
+    <h2>Ecosystem</h2>
+    <ul>
+      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import logo from './Logo.vue'
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components: { logo }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Logo.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="logo-wrapper">
+    <div class="logo">
+      <span class="large">A</span><span class="small">b</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'logo',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  .logo-wrapper {
+    margin: 0 auto;
+    font: 50px/1.6 Arial, sans-serif;
+    .logo {
+      width: 1.6em;
+      height: 1.6em;
+      padding: .1em;
+      font-weight: bolder;
+      color: #41B883;
+      border: .1em solid #41B883;
+      background-color: #35495E;
+      /*border-radius: 50%;*/
+    }
+    .large {
+      font-size: 1em;
+    }
+    .small {
+      font-size: .76em;
+    }
+  }
+</style>
